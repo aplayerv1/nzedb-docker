37ea2b78eff60665eda97f7c831eb4e98741b3ed
APlayer <hoyhoy100@hotmail.com>
2019-10-30 17:18:47 -0400

    hi

--- rootfs/opt/scripts/refresh.sh
+++ rootfs/opt/scripts/refresh.sh
@@ -6,10 +6,10 @@ REFRESH_POSTPROCESS_OPTIONS="${_REFRESH_POSTPROCESS_OPTIONS:-nfo mov tv ama}";
 until [[ -f "/opt/http/configuration/install.lock" ]]; do
     sleep 10;
 done
-if [ -f "/opt/http/configuration/install.lock" ]; then
-    echo "Importing predb"
-    su - www-data -s /bin/bash -c "$(which php)  /opt/http/cli/data/predb_import_daily_batch.php 0 local true"
-fi
+# until [[ -f "/opt/http/configuration/install.lock" ]]; do
+#     echo "Importing predb"
+#     su - www-data -s /bin/bash -c "$(which php)  /opt/http/cli/data/predb_import_daily_batch.php 0 local true"
+# done
 
 while (true); do
     su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/multiprocessing/binaries.php 0";
