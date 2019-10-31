5bd2eca83ffec527df8de77715e59141157c96e1
APlayer <hoyhoy100@hotmail.com>
2019-10-31 09:13:09 -0400

    hi

--- rootfs/opt/scripts/refresh.sh
+++ rootfs/opt/scripts/refresh.sh
@@ -24,5 +24,6 @@ if (true); then
         echo "screen is running."
     else
         screen -d -m -S tmux bash -c 'su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/tmux/start.php"'
+    fi
 fi
  
