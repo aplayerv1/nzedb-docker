a899438dfbac87a641ed29428248c99393040ace
APlayer <hoyhoy100@hotmail.com>
2019-10-31 10:39:32 -0400

    hi

--- rootfs/opt/scripts/refresh.sh
+++ rootfs/opt/scripts/refresh.sh
@@ -22,10 +22,16 @@ done
 if (true); then
     if ! screen -list | grep -q "tmux"; then
         echo "Preparing Life"
-        cp /opt/http/misc/nix/screen/squential/threaded.sh /opt/http/misc/nix/screen/squential/user_threaded.sh
-        screen -d -m -S tmux /bin/bash -c 'su - www-data -s /bin/bash -c php /opt/http/misc/nix/tmux/start.php'
+        cp /opt/http/misc/update/nix/screen/sequential/threaded.sh /opt/http/misc/update/nix/screen/sequential/user_threaded.sh
+        cd /opt/http/misc/update/nix/tmux
+        screen -d -m -S tmux /bin/bash -c 'php start.php'
+    elif screen -list | grep -q "dead"; then
+        echo "screen dead"
+        screen -wipe
     else
        echo "screen is running"
     fi
+    sleep 60;
 fi
+
  
