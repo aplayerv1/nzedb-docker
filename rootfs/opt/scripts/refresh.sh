576d8cbd2be21913dd5f0741c558ebb9c8f570cf
APlayer <hoyhoy100@hotmail.com>
2019-10-31 07:26:24 -0400

    hi

--- rootfs/opt/scripts/refresh.sh
+++ rootfs/opt/scripts/refresh.sh
@@ -11,14 +11,14 @@ done
 #     su - www-data -s /bin/bash -c "$(which php)  /opt/http/cli/data/predb_import_daily_batch.php 0 local true"
 # done
 
-while (true); do
-    su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/multiprocessing/binaries.php 0";
-    su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/multiprocessing/releases.php"
-    for pparm in ${REFRESH_POSTPROCESS_OPTIONS}; do
-        su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/multiprocessing/postprocess.php $pparm";
-    done
-    sleep 60;
-# if (true); then
-#     su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/tmux/start.php"
-done
+# while (true); do
+#     su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/multiprocessing/binaries.php 0";
+#     su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/multiprocessing/releases.php"
+#     for pparm in ${REFRESH_POSTPROCESS_OPTIONS}; do
+#         su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/multiprocessing/postprocess.php $pparm";
+#     done
+#     sleep 60;
+if (true); then
+    su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/tmux/start.php"
+fi
  
