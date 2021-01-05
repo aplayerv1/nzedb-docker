264ccb1cf253789adc1d651bad4dd9c1fabfde17
APlayer <hoyhoy100@hotmail.com>
2021-01-05 12:06:48 -0500

    asdasdasdasd

--- rootfs/opt/scripts/install_nzedb.sh
+++ rootfs/opt/scripts/install_nzedb.sh
@@ -14,22 +14,19 @@ echo "=== Cloning nZEDb from https://github.com/nZEDb/nZEDb.git to ${_PATH_INSTA
 echo ""
 git clone https://github.com/nZEDb/nZEDb.git ${_PATH_INSTALL_ROOT} || exit 5;
 #composer create-project --no-dev --keep-vcs --prefer-source nzedb/nzedb ${_PATH_INSTALL_ROOT} || exit 5;
-cd /opt/http
-wget https://getcomposer.org/download/1.10.19/composer.phar
-php composer.phar install --prefer-source
 chmod -R 755 /opt/html/app/libraries && chmod -R 755 /opt/html/libraries && chmod -R 777 /opt/html/resources && chmod -R 777 /opt/html/www
 echo ""
 echo "--- Done."
 echo ""
 echo "=== Running composer install on nZEDb directory."
 echo ""
-#composer --no-ansi install || exit 6;
+composer --no-ansi install || exit 6;
 echo ""
 echo "--- Done."
 echo ""
 echo "=== Installing additional modules required."
 echo ""
-#composer --no-ansi require league/oauth2-google || exit 7;
+composer --no-ansi require league/oauth2-google || exit 7;
 echo ""
 echo "--- Done."
 exit 0;
