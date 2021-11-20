7bb3e0b1ec1fda25b2b2308edde4699c2d692818
unknown <hoyhoy100@hotmail.com>
2021-11-20 07:43:49 -0500

    aa

--- rootfs/opt/scripts/refresh.sh
+++ rootfs/opt/scripts/refresh.sh
@@ -11,15 +11,6 @@ until [[ -f "/opt/http/configuration/install.lock" ]]; do
      su - www-data -s /bin/bash -c "$(which php)  /opt/http/cli/data/predb_import_daily_batch.php 0 local true"
 done
 
-# while (true); do
-#     su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/multiprocessing/binaries.php 0";
-#     su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/multiprocessing/releases.php"
-#     for pparm in ${REFRESH_POSTPROCESS_OPTIONS}; do
-#         su - www-data -s /bin/bash -c "$(which php) /opt/http/misc/update/nix/multiprocessing/postprocess.php $pparm";
-#     done
-#     sleep 60;
-#done
-
 if (true); then
     if ! screen -list | grep -q "tmux"; then
         echo "Preparing Life"
@@ -30,13 +21,12 @@ if (true); then
         echo "screen dead"
         screen -wipe
     else
-       echo "screen is running"
        echo "Checking if script is running"
-       if tail -n 10 /opt/http/resources/logs/php_errors_cli.log | grep 'Connection refused in /opt/http/libraries/lithium/data/source/Database.php'; then
-          echo "Error Detected restarting tmux"
-          pkill tmux
-       else
-          echo "no errors"
+       value=$(tmux list-panes -a -F "#{pane_dead} #{pane_id}" | grep "^1" | head -1 | awk '{print $2}')
+       if [ "$value" = "%0" ]; then
+        printf "its dead jim"
+        printf "\n" "restarting"
+        pkill tmux
        fi
     fi
     sleep 60;
